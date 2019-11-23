a2c30ac8eb3a4b57224a5c6eda705e366874ab30
Ben Kim <benkimo6i@gmail.com>
2019-11-23 12:57:52 -0800

    style carousel

---
+++ carousel.js
@@ -0,0 +1,8 @@
+let galleryItems = document.querySelectorAll('.gallery-item');
+
+galleryItems[0].classList.add('gallery-item-first');
+galleryItems[1].classList.add('gallery-item-previous');
+galleryItems[2].classList.add('gallery-item-selected');
+galleryItems[3].classList.add('gallery-item-next');
+galleryItems[4].classList.add('gallery-item-last');
+
